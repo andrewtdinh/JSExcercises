7377da7b928d09b73b0bab711738c30a6716b780
Andrew Dinh <andrewdinh74@yahoo.com>
2015-10-08 23:58:11 -0700

    Define a function to determine the date suffix

--- dateTime.js
+++ dateTime.js
@@ -3,20 +3,23 @@ var dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
 var dateSuffix = 'th';
 var date = today.getDate();
 
-switch ((date % 30) % 20){
-  case 1:
-    dateSuffix = 'st';
-    break;
-  case 2:
-    dateSuffix = 'nd';
-    break;
-  case 3:
-    dateSuffix = 'rd';
+function getDateSuffix(d){
+  switch ((d % 30) % 20){
+    case 1:
+      dateSuffix = 'st';
+      break;
+    case 2:
+      dateSuffix = 'nd';
+      break;
+    case 3:
+      dateSuffix = 'rd';
+  }
+  return dateSuffix;
 }
-console.log('Today is: ' +dayList[today.getDay()]+ ', the ' +date+ dateSuffix);
+console.log('Today is: ' +dayList[today.getDay()]+ ', the ' +date+ getDateSuffix(date));
 dates = [1, 2, 3, 21, 22, 23, 31]
 dates.forEach(function(localDate) {
-  console.log('Today is: ' +dayList[today.getDay()]+ ', the ' +localDate+ dateSuffix);  
+  console.log('Today is: ' +dayList[today.getDay()]+ ', the ' +localDate+ getDateSuffix(localDate));  
 });
 
 
