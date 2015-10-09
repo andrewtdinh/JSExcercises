3b24f9d108c91555a44f3cf2ed784551563ab12c
Andrew Dinh <andrewdinh74@yahoo.com>
2015-10-09 16:37:43 -0700

    Finish the date time exercise

--- dateTime.js
+++ dateTime.js
@@ -22,9 +22,21 @@ function getDateSuffix(d){
 console.log('Today is: ' +dayList[today.getDay()]+ ', the ' +date+ getDateSuffix(date));
 dates = [1, 2, 3, 21, 22, 23, 31, 12, 13]
 dates.forEach(function(localDate) {
-  console.log('Checking date suffixes:  ' +localDate+ getDateSuffix(localDate));  
+  console.log('Checking date suffixes:  ' +localDate+ getDateSuffix(localDate));
 });
-
-
-
-
+var hours = today.getHours();
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
+var hourSuffix;
+if ((today/1000) % 43200 === 0){
+  if (hours === 12){hourSuffix = 'Noon'}
+  else {hourSuffix = 'Midnight'}
+}
+else {
+  if (hours < 12){hourSuffix = 'AM'}
+  else {
+    hourSuffix = 'PM';
+    hours = hours - 12;
+  }
+}
+console.log('Current time is:  ' +hours+ ' ' +hourSuffix+ ' : ' +minutes+ ' : ' +seconds);
